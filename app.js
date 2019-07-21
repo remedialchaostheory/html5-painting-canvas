b0310306ecc70b8513b1cd21a79403874051173d
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-20 21:04:11 -0700

    Add canvas context

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 (function () {
   'use strict';
+  const canvas = document.querySelector('#draw');
+  const cxt = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
 
 })();
