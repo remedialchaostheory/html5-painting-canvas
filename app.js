ecec0753b8cb0d6693a2fb712deda8e8ad41b5f9
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-24 21:28:04 -0700

    Add paintbrush tip default settings

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 (function () {
   'use strict';
   const canvas = document.querySelector('#draw');
-  const cxt = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  ctx.strokeStyle = '#ffc600';
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+
 
 
 })();
